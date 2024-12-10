6a2e0e2303af53fc238d3b6302a1c4238cbb34d3
Blen Redwan <blenredwan@gmail.com>
2024-12-10 16:13:31 +0300

    update

--- stack/scoreOfParentheses.py
+++ stack/scoreOfParentheses.py
@@ -1,3 +1,27 @@
+'''856. Score of Parentheses
+
+Given a balanced parentheses string s, return the score of the string.
+
+The score of a balanced parentheses string is based on the following rule:
+
+"()" has score 1.
+AB has score A + B, where A and B are balanced parentheses strings.
+(A) has score 2 * A, where A is a balanced parentheses string.
+ 
+
+Example 1:
+
+Input: s = "()"
+Output: 1
+Example 2:
+
+Input: s = "(())"
+Output: 2
+Example 3:
+
+Input: s = "()()"
+Output: 2
+ '''
 class Solution:
     def scoreOfParentheses(self, s: str) -> int:
         stack = [0]
@@ -12,4 +36,5 @@ class Solution:
                 else:
                     stack[-1] += a*2
         return stack[0]
+    
     
